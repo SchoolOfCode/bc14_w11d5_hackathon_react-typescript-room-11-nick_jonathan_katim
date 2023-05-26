ab4ac1bf697595a5771fb5154519dbacc76379d4
Grego12098 <jgregory12098@gmail.com>
2023-05-26 12:04:33 +0100

    search bar set up

---
+++ weather-app/src/components/api/GetCityWeather.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+type GetCityWeatherProps = {
+    city: string;
+}
+export default function GetCityWeather(props: GetCityWeatherProps) {
+    const {data, isLoading, isError, error} = useQuery({
+        queryKey: ['data'], 
+        queryFn: async () => {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=0c63ca9f419924f3e5badb3b4760bb53`;
+            const { data } = await axios.get(url);
+            console.log('test');
+            return data;
+        }
+        });
+    
+        if (isLoading) {
+            return <div>Loading...</div>;
+        }
+    
+        if (isError) {
+            return <div>Error: {error.message}</div>;
+        }
+    
+      return (
+          <div>{JSON.stringify(data)}</div>
+      );
+}
