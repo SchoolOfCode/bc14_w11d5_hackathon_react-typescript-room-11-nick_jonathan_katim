ad17db234a047c03e2b4938a6df8e8c423aab1b0
Katim Touray <katim70@hotmail.co.uk>
2023-05-26 13:44:47 +0100

    UK specificity

--- weather-app/src/components/api/GetCityWeather.tsx
+++ weather-app/src/components/api/GetCityWeather.tsx
@@ -5,10 +5,10 @@ type GetCityWeatherProps = {
     city: string;
 }
 export default function GetCityWeather(props: GetCityWeatherProps) {
-    const {data, isLoading, isError, error} = useQuery({
+    const { data: weatherData, isLoading, isError, error} = useQuery({
         queryKey: ['data'], 
         queryFn: async () => {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=0c63ca9f419924f3e5badb3b4760bb53`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city},UK&appid=0c63ca9f419924f3e5badb3b4760bb53`;
             const { data } = await axios.get(url);
             console.log('test');
             return data;
@@ -24,6 +24,6 @@ export default function GetCityWeather(props: GetCityWeatherProps) {
         }
     
       return (
-          <div>{JSON.stringify(data)}</div>
+          <div>{JSON.stringify(weatherData)}</div>
       );
 }
